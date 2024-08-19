74cacb13b3da74a4b5ad9f20acbaf801b66f2f8a
MalikMohsin5885 <malikmohsin8239@gmail.com>
2024-08-19 15:53:27 +0500

    image url updated

--- src/data/data.js
+++ src/data/data.js
@@ -11,14 +11,14 @@ export const data = [
   // },
   {
     id: 2,
-    img: './src/assets/reservify.png',
+    img: 'https://drive.google.com/file/d/19AFXoPtt1y5GRjnZnfeyMjM3SLNizfZa/view',
     title: "Reservify",
     link: "",
     git: "https://github.com/MalikMohsin5885/Reservify",
   },
   {
     id: 3,
-    img: './src/assets/healthcare.png',
+    img: 'https://drive.google.com/file/d/1s94HHKxpyb9UV1N9NgelnlB-8fbKmedt/view',
     title: "Health Care Portal",
     desc: "",
     link: "",
